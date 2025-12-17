a9a96b42c2a8050d00313ce681d0b09773d029b3
Kingewor9 <michealewor9@gmail.com>
2025-12-17 09:36:55 +0100

    Adjusted the pb and text in the configure channel section

--- frontend/src/pages/AddChannelPage.tsx
+++ frontend/src/pages/AddChannelPage.tsx
@@ -1,6 +1,4 @@
-
-
-         import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Layout from '../components/Layout';
 import ErrorAlert from '../components/ErrorAlert';
