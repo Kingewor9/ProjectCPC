7753634d8abdf626a59908629ad26e13e0dba1c9
Kingewor9 <michealewor9@gmail.com>
2025-12-29 21:22:09 +0100

    made a typo change

--- frontend/src/pages/RequestsPage.tsx
+++ frontend/src/pages/RequestsPage.tsx
@@ -404,7 +404,7 @@ export default function RequestsPage() {
           </div>
         )}
 
-        {/* NEW: Decline Modal */}
+        {/* Decline Modal */}
         {showDeclineModal && selectedRequest && (
           <div className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 p-4">
             <div className="bg-darkBlue-800 border border-grey-700 rounded-xl max-w-md w-full p-4 sm:p-6">
